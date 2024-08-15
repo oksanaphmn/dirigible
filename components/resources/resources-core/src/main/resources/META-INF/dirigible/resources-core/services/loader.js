a45e3fce5998c7b1212168e572054331bbce07fe
StanZGenchev <stan.z.genchev@gmail.com>
2024-08-15 19:20:08 +0300

    Fixed loader
    Signed-off-by: StanZGenchev <stan.z.genchev@gmail.com>

--- components/resources/resources-core/src/main/resources/META-INF/dirigible/resources-core/services/loader.js
+++ components/resources/resources-core/src/main/resources/META-INF/dirigible/resources-core/services/loader.js
@@ -9,35 +9,14 @@
  * SPDX-FileCopyrightText: Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
-import { request, response } from "sdk/http";
-import { registry } from "sdk/platform";
-import { uuid } from "sdk/utils";
+import { request, response } from 'sdk/http';
+import { registry } from 'sdk/platform';
+import { uuid } from 'sdk/utils';
 
-const COOKIE_PREFIX = "DIRIGIBLE.resources-core.loader.";
+const COOKIE_PREFIX = 'DIRIGIBLE.resources-core.loader.';
 
-const baseJs = [
-    "/jquery/3.7.1/jquery.min.js",
-    "/angularjs/1.8.2/angular.min.js",
-    "/angularjs/1.8.2/angular-resource.min.js",
-    "/angular-aria/1.8.2/angular-aria.min.js",
-    "/split.js/1.6.5/dist/split.min.js",
-    "/resources-core/core/uri-builder.js",
-    "/resources-core/core/message-hub.js",
-    "/resources-core/core/ide-message-hub.js",
-    "/resources-core/ui/theming.js",
-    "/resources-core/ui/widgets.js",
-    "/resources-core/ui/extensions.js",
-    "/resources-core/ui/view.js",
-];
-
-const viewCss = [
-    "/fundamental-styles/0.37.4/dist/fundamental-styles.css",
-    "/resources/styles/core.css",
-    "/resources/styles/widgets.css",
-];
-
-const scriptId = request.getParameter("id");
-const scriptVersion = request.getParameter("v");
+const scriptId = request.getParameter('id');
+const scriptVersion = request.getParameter('v');
 if (scriptId) {
     if (isCached(scriptId)) {
         responseNotModified();
@@ -45,7 +24,7 @@ if (scriptId) {
         processScriptRequest(scriptId);
     }
 } else {
-    responseBadRequest("Provide the 'id' parameter of the script");
+    responseBadRequest('Provide the \'id\' parameter of the script');
 }
 
 response.flush();
@@ -61,7 +40,7 @@ function setETag(scriptId) {
         'path': '/',
         'maxAge': maxAge
     });
-    response.setHeader("ETag", etag);
+    response.setHeader('ETag', etag);
     response.setHeader('Cache-Control', `private, must-revalidate, max-age=${maxAge}`);
 }
 
@@ -76,7 +55,7 @@ function isCached(scriptId) {
         cookie = cookies.filter(e => e.name === getCacheKey(scriptId))[0];
     }
     if (cookie) {
-        return cookie.value === request.getHeader("If-None-Match");
+        return cookie.value === request.getHeader('If-None-Match');
     }
     return false;
 }
@@ -84,105 +63,183 @@ function isCached(scriptId) {
 function processScriptRequest(scriptId) {
     let locations = getLocations(scriptId);
     if (locations) {
-        let contentType = scriptId.endsWith('-js') ? "text/javascript;charset=UTF-8" : "text/css";
+        let contentType = scriptId.endsWith('-js') ? 'text/javascript;charset=UTF-8' : 'text/css';
         response.setContentType(contentType);
 
         setETag(scriptId);
         locations.forEach(function (scriptLocation) {
-            response.println(registry.getText(scriptLocation));
+            let text = registry.getText(scriptLocation);
+            if (text.includes('//# sourceMappingURL=')) {
+                text = text.replace('//# sourceMappingURL=', `//# sourceMappingURL=/webjars${scriptLocation.slice(0, scriptLocation.lastIndexOf('/') + 1)}`)
+            }
+            response.println(text);
         });
     } else {
-        responseBadRequest("Script with 'id': " + scriptId + " is not known.");
+        responseBadRequest(`Script with id '${scriptId}' is not known.`);
     }
 }
 
 function getLocations(scriptId) {
     if (scriptVersion) {
-        const base = [
-            "/jquery/3.7.1/jquery.min.js",
-            "/angularjs/1.8.2/angular.min.js",
-            "/angularjs/1.8.2/angular-resource.min.js",
-            "/angular-aria/1.8.2/angular-aria.min.js",
-            "/split.js/1.6.5/dist/split.min.js",
-            "/resources-core/core/uri-builder.js",
-            "/resources-core/core/message-hub.js",
-            "/resources-core/ui/platform/user.js",
-            "/resources-core/ui/platform/brand.js",
-            "/resources-core/ui/platform/messageHub.js",
-            "/resources-core/ui/platform/theming.js",
-            "/resources-core/ui/platform/extensions.js",
-            "/resources-core/ui/platform/view.js",
-            "/resources-core/ui/blimpkit/widgets.js",
-        ]
+        const baseJs = [
+            '/jquery/3.6.0/jquery.min.js',
+            '/angularjs/1.8.2/angular.min.js',
+            '/angularjs/1.8.2/angular-resource.min.js',
+            '/angular-aria/1.8.2/angular-aria.min.js',
+            '/split.js/1.6.5/dist/split.min.js',
+            '/ide-branding/branding.js',
+            '/resources-core/core/uri-builder.js',
+            '/resources-core/core/message-hub.js',
+            '/resources-core/ui/platform/user.js',
+            '/resources-core/ui/platform/messageHub.js',
+            '/resources-core/ui/platform/theming.js',
+            '/resources-core/ui/platform/extensions.js',
+            '/resources-core/ui/platform/view.js',
+            '/resources-core/ui/blimpkit/blimpkit.js',
+            '/resources-core/ui/blimpkit/avatar.js',
+            '/resources-core/ui/blimpkit/badge.js',
+            '/resources-core/ui/blimpkit/bar.js',
+            '/resources-core/ui/blimpkit/busy-indicator.js',
+            '/resources-core/ui/blimpkit/button.js',
+            '/resources-core/ui/blimpkit/card.js',
+            '/resources-core/ui/blimpkit/checkbox.js',
+            '/resources-core/ui/blimpkit/combobox.js',
+            '/resources-core/ui/blimpkit/dialog.js',
+            '/resources-core/ui/blimpkit/forms.js',
+            '/resources-core/ui/blimpkit/input.js',
+            '/resources-core/ui/blimpkit/link.js',
+            '/resources-core/ui/blimpkit/radio.js',
+            '/resources-core/ui/blimpkit/icon-tab-bar.js',
+            '/resources-core/ui/blimpkit/list.js',
+            '/resources-core/ui/blimpkit/menu.js',
+            '/resources-core/ui/blimpkit/message-page.js',
+            '/resources-core/ui/blimpkit/object-status.js',
+            '/resources-core/ui/blimpkit/pagination.js',
+            '/resources-core/ui/blimpkit/panel.js',
+            '/resources-core/ui/blimpkit/popover.js',
+            '/resources-core/ui/blimpkit/product-switch.js',
+            '/resources-core/ui/blimpkit/scrollbar.js',
+            '/resources-core/ui/blimpkit/select.js',
+            '/resources-core/ui/blimpkit/step-input.js',
+            '/resources-core/ui/blimpkit/table.js',
+            '/resources-core/ui/blimpkit/textarea.js',
+            '/resources-core/ui/blimpkit/tile.js',
+            '/resources-core/ui/blimpkit/token.js',
+            '/resources-core/ui/blimpkit/tokenizer.js',
+            '/resources-core/ui/blimpkit/tool-header.js',
+            '/resources-core/ui/blimpkit/toolbar.js',
+            '/resources-core/ui/blimpkit/upload-collection.js',
+            '/resources-core/ui/blimpkit/vertical-navigation.js',
+            '/resources-core/ui/blimpkit/wizard.js'
+        ];
+        const viewCss = [
+            '/fundamental-styles/0.30.2/dist/fundamental-styles.css',
+            '/resources/styles/blimpkit.css',
+        ];
         switch (scriptId) {
-            case "platform-view-js":
-                return base;
-            case "platform-shell-js":
+            case 'view-js':
+                return baseJs;
+            case 'perspective-js':
                 return [
-                    "/ide-branding/branding.js",
-                    ...base,
-                    "/angularjs/1.8.2/angular-cookies.min.js",
-                    "/resources-core/ui/shell/core.js",
+                    ...baseJs,
+                    '/angularjs/1.8.2/angular-cookies.min.js',
+                    '/resources-core/ui/platform/editors.js',
+                    '/resources-core/ui/platform/layout.js',
                 ];
-            case "file-upload-js":
+            case 'shell-js':
                 return [
-                    "/es5-shim/4.6.7/es5-shim.min.js",
-                    "/angular-file-upload/2.6.1/dist/angular-file-upload.min.js",
+                    ...baseJs,
+                    '/angularjs/1.8.2/angular-cookies.min.js',
+                    '/resources-core/ui/platform/brand.js',
+                    '/resources-core/ui/platform/dialogs.js',
+                    '/resources-core/ui/platform/shell.js',
                 ];
-            case "sanitize-js":
-                return ["/angularjs/1.8.2/angular-sanitize.min.js"];
-            case "platfrom-view-css":
+            case 'file-upload-js':
+                return [
+                    '/es5-shim/4.6.7/es5-shim.min.js',
+                    '/angular-file-upload/2.6.1/dist/angular-file-upload.min.js',
+                ];
+            case 'sanitize-js':
+                return ['/angularjs/1.8.2/angular-sanitize.min.js'];
+            case 'split-js':
+                return ['/split.js/1.6.5/dist/split.min.js', '/resources-core/ui/platform/split.js'];
+            case 'split-css':
+                return ['/resources/styles/split.css'];
+            case 'view-css':
                 return viewCss;
-            case "platform-shell-css":
-            case "platfrom-perspective-css":
-                return [...viewCss, "/resources/styles/layout.css", "/resources/styles/perspective.css"]
-            case "code-editor-js":
-                return ["/ide-monaco/embeddable/editor.js", "/monaco-editor/0.40.0/min/vs/loader.js", "/monaco-editor/0.40.0/min/vs/editor/editor.main.nls.js", "/monaco-editor/0.40.0/min/vs/editor/editor.main.js"];
-            case "code-editor-css":
-                return ["/ide-monaco/css/embeddable.css", "/monaco-editor/0.40.0/min/vs/editor/editor.main.css"];
+            case 'shell-css':
+            case 'perspective-css':
+                return [...viewCss, '/resources/styles/split.css', '/resources/styles/layout.css', '/resources/styles/perspective.css']
+            case 'code-editor-js':
+                return ['/ide-monaco/embeddable/editor.js', '/monaco-editor/0.40.0/min/vs/loader.js', '/monaco-editor/0.40.0/min/vs/editor/editor.main.nls.js', '/monaco-editor/0.40.0/min/vs/editor/editor.main.js'];
+            case 'code-editor-css':
+                return ['/ide-monaco/css/embeddable.css', '/monaco-editor/0.40.0/min/vs/editor/editor.main.css'];
         }
     } else {
+        const baseJs = [
+            '/jquery/3.6.0/jquery.min.js',
+            '/angularjs/1.8.2/angular.min.js',
+            '/angularjs/1.8.2/angular-resource.min.js',
+            '/angular-aria/1.8.2/angular-aria.min.js',
+            '/split.js/1.6.5/dist/split.min.js',
+            '/resources-core/core/uri-builder.js',
+            '/resources-core/core/message-hub.js',
+            '/resources-core/core/ide-message-hub.js',
+            '/resources-core/ui/theming.js',
+            '/resources-core/ui/widgets.js',
+            '/resources-core/ui/extensions.js',
+            '/resources-core/ui/view.js',
+        ];
+        const viewCss = [
+            '/fundamental-styles/0.30.2/dist/fundamental-styles.css',
+            '/resources/styles/core.css',
+            '/resources/styles/widgets.css',
+        ];
         switch (scriptId) {
-            case "application-view-js":
-            case "ide-view-js":
+            case 'application-view-js':
+            case 'ide-view-js':
                 return [
                     ...baseJs,
-                    "/resources-core/ui/entityApi.js",
+                    '/resources-core/ui/entityApi.js',
                 ];
-            case "ide-editor-js":
+            case 'ide-editor-js':
                 return [
                     ...baseJs,
-                    "/resources-core/ui/entityApi.js",
-                    "/ide-workspace-service/workspace.js",
+                    '/resources-core/ui/entityApi.js',
+                    '/ide-workspace-service/workspace.js',
                 ]
-            case "application-perspective-js":
-            case "ide-perspective-js":
+            case 'application-perspective-js':
+            case 'ide-perspective-js':
                 return [
                     ...baseJs,
-                    "/angularjs/1.8.2/angular-cookies.min.js",
-                    "/ide-branding/branding.js",
-                    "/resources-core/ui/editors.js",
-                    "/resources-core/ui/core-modules.js",
-                    "/resources-core/ui/layout.js",
+                    '/angularjs/1.8.2/angular-cookies.min.js',
+                    '/ide-branding/branding.js',
+                    '/resources-core/ui/editors.js',
+                    '/resources-core/ui/core-modules.js',
+                    '/resources-core/ui/layout.js',
                 ];
-            case "file-upload-js":
+            case 'file-upload-js':
                 return [
-                    "/es5-shim/4.6.7/es5-shim.min.js",
-                    "/angular-file-upload/2.6.1/dist/angular-file-upload.min.js",
+                    '/es5-shim/4.6.7/es5-shim.min.js',
+                    '/angular-file-upload/2.6.1/dist/angular-file-upload.min.js',
                 ];
-            case "sanitize-js":
-                return ["/angularjs/1.8.2/angular-sanitize.min.js"];
-            case "application-view-css":
-            case "ide-editor-css":
-            case "ide-view-css":
+            case 'sanitize-js':
+                return ['/angularjs/1.8.2/angular-sanitize.min.js'];
+            case 'split-js':
+                return ['/split.js/1.6.5/dist/split.min.js', '/resources-core/ui/platform/split.js'];
+            case 'split-css':
+                return ['/resources/styles/split.css'];
+            case 'application-view-css':
+            case 'ide-editor-css':
+            case 'ide-view-css':
                 return viewCss;
-            case "application-perspective-css":
-            case "ide-perspective-css":
-                return [...viewCss, "/resources/styles/layout.css", "/resources/styles/perspective.css"]
-            case "code-editor-js":
-                return ["/ide-monaco/embeddable/editor.js", "/monaco-editor/0.40.0/min/vs/loader.js", "/monaco-editor/0.40.0/min/vs/editor/editor.main.nls.js", "/monaco-editor/0.40.0/min/vs/editor/editor.main.js"];
-            case "code-editor-css":
-                return ["/ide-monaco/css/embeddable.css", "/monaco-editor/0.40.0/min/vs/editor/editor.main.css"];
+            case 'application-perspective-css':
+            case 'ide-perspective-css':
+                return [...viewCss, '/resources/styles/layout.css', '/resources/styles/split.css', '/resources/styles/perspective.css']
+            case 'code-editor-js':
+                return ['/ide-monaco/embeddable/editor.js', '/monaco-editor/0.40.0/min/vs/loader.js', '/monaco-editor/0.40.0/min/vs/editor/editor.main.nls.js', '/monaco-editor/0.40.0/min/vs/editor/editor.main.js'];
+            case 'code-editor-css':
+                return ['/ide-monaco/css/embeddable.css', '/monaco-editor/0.40.0/min/vs/editor/editor.main.css'];
         }
     }
 }
@@ -192,7 +249,7 @@ function responseNotModified() {
 }
 
 function responseBadRequest(message) {
-    response.setContentType("text/plain");
+    response.setContentType('text/plain');
     response.setStatus(response.BAD_REQUEST);
     response.println(message);
 }
